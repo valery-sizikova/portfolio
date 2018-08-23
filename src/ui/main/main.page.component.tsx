ec048c1401788271634a5fc8f5bfd8102ab55cb5
DESKTOP-EAI5F7M\valery.sizikova <valerysizikova@semmtech.nl>
2018-08-23 13:38:33 +0200

    added scrolling library, added font

--- src/ui/main/main.page.component.tsx
+++ src/ui/main/main.page.component.tsx
@@ -1,34 +1,73 @@
 import * as React from 'react';
-import { resolveInject } from './../../di';
+import { Menu, MenuItem } from 'react-foundation';
+import { Element, Link } from "react-scroll";
 
-import { IMainService } from '../../services/main/main.service';
+class Section {
+  label: string;
+  id: string;
+}
 
 interface IComponentProps { }
 
 interface IComponentState {
-  pageTitle: string;
+  sections: Section[];
 }
 
 export default class MainPageComponent extends React.Component<IComponentProps, IComponentState> {
-  private mainService = resolveInject(IMainService);
   constructor(props: IComponentProps) {
     super(props);
     this.state = {
-      pageTitle: ""
+      sections: [
+        {
+          label: "Section 1",
+          id: "section_1"
+        },
+        {
+          label: "Section 2",
+          id: "section_2"
+        },
+        {
+          label: "Section 3",
+          id: "section_3"
+        },
+        {
+          label: "Section 4",
+          id: "section_4"
+        }
+      ]
     };
   }
 
-  componentDidMount() {
-    const text = this.mainService.getPageTitle();
-    this.setState({
-      pageTitle: text
-    })
+  handleSetActive(to: string) {
+    console.log(to);
   }
 
   render() {
     return (
       <div className="main-container">
-        <p>{this.state.pageTitle}</p>
+        <Menu className="navbar" isVertical>
+          {this.state.sections.map(x => <MenuItem
+            key={`link_to_${x.id}`}
+          >
+            <Link
+              activeClass="active"
+              to={x.id}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+              onSetActive={this.handleSetActive}>
+              {x.label}
+            </Link>
+          </MenuItem>)}
+        </Menu>
+        {this.state.sections.map(x =>
+          <Element key={`section_${x.id}`} name={x.id} className="element">
+            <section className="section-big">
+              <h4>{x.label}</h4>
+            </section>
+          </Element>
+        )}
       </div>
     );
   }
