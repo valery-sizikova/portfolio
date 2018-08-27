656829fc3670aca2b8fd8214f41002cdba5627e8
DESKTOP-EAI5F7M\valery.sizikova <valerysizikova@semmtech.nl>
2018-08-27 18:06:15 +0200

    removed services

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
-import DependecyInjectionSetup from './di-setup';
 import { Routes } from 'src/routes';
 
 import './styles.css';
-
-DependecyInjectionSetup.setup();
 
 ReactDOM.render(
   <Router>
--- src/services/main/main.service.test.ts
+++
@@ -1,8 +0,0 @@
-import { MainService } from './main.service';
-
-describe('Main service', () => {
-  it('gets the page title', () => {
-    const mainService = new MainService();
-    expect(mainService.getPageTitle()).toMatch(/Main page/)
-  })
-})
